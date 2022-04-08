fd515d4552007b0a3cc8d64a80ff347457bfbdd4
sop <szhan253@ucsc.edu>
2022-04-07 22:12:29 -0700

    add all assets

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -2,4 +2,9 @@ class Menu extends Phaser.Scene {
     constructor() {
         super("menuScene");
     }
+
+    create() {
+        this.add.text(20, 20, "Rocket Patrol Menu");
+        this.scene.start("playScene");
+      }
 }
---
+++ src/scenes/Play.js
@@ -0,0 +1,15 @@
+class Play extends Phaser.Scene {
+    constructor() {
+      super("playScene");
+    }
+    
+    create() {
+        // green UI background
+        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
+        // white borders
+        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+    }
+  }
